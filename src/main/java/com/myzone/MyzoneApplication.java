e3a7622599b3179ebb5d6cc3d85d6a4d5a6536b1
单剑辉 <shanjianhui@SJH-MBP.workgroup>
2017-01-02 22:45:49 +0800

    Initial commit

---
+++ src/main/java/com/myzone/MyzoneApplication.java
@@ -0,0 +1,12 @@
+package com.myzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyzoneApplication.class, args);
+	}
+}
