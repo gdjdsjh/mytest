ff1d981f6c77a835f52ee4da04b64220a6269fbd
shanjianhui <gdjdsjh@126.com>
2017-01-02 23:15:16 +0800

    Controller

--- src/main/java/com/myzone/controller/IndexController.java
+++ src/main/java/com/myzone/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.myzone.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by shanjianhui on 2017/1/2.
  */
+@Controller
 public class IndexController {
 
     @RequestMapping(path = {"/"})
